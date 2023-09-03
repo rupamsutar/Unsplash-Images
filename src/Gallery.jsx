8cb8f458ff5da8de66a61a9c55245cf20e83b94b
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-03 23:24:52 +0530

    DarkMode Javascript check using matchMedia.

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -10,7 +10,7 @@ const Gallery = () => {
     const {searchTerm} = useGlobalContext();
 
     const response = useQuery({
-        queryKey: ['images'],
+        queryKey: ['images', searchTerm],
         queryFn: async() => {
             const result = await axios.get(`${url}&query=${searchTerm}`);
 
