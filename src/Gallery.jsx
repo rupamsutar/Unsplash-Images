80142bebe2bcb3dbe12d880f56ff5a2867621b6c
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-03 22:41:03 +0530

    Setup Query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const url = "https://api.unsplash.com/search/photos?client_id=HiEU_-DkM-7wCIwXyOuXV5i8H0HNgTGZ8UuXJvncdKk&query=office";
 
 const Gallery = () => {
+
+    const response = useQuery({
+        queryKey: ['images'],
+        queryFn: async() => {
+            const result = await axios.get(url);
+
+            return result;
+        }
+    })
+
+    console.log(response.data);
+
   return (
     <h2>Gallery</h2>
   )
