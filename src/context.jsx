39b9b694d5d5ec9d23c2b09ef794ebeb1e89b79d
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-03 19:41:29 +0530

    Dark Theme CSS

--- src/context.jsx
+++ src/context.jsx
@@ -9,6 +9,10 @@ export const AppProvider = ({ children }) => {
     const toggleDarkTheme = () => {
         const newDarkTheme = !isDarkTheme;
         setIsDarkTheme(newDarkTheme);
+
+        const body = document.querySelector('body');
+        body.classList.toggle('dark-theme', newDarkTheme);
+        console.log(body);
     }
 
     return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme}}>{children}</AppContext.Provider>
